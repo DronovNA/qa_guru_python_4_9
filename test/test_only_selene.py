53b62440d753a5dbd92975d43e7b3e37ade54b16
DronovNA <nikita.dronov.a@gmail.com>
2023-05-10 01:24:52 +0300

    done hw

--- test/test_only_selene.py
+++ test/test_only_selene.py
@@ -3,7 +3,7 @@ from selene import browser
 
 
 def test_github():
-    browser.open_url("https://github.com")
+    browser.open("https://github.com")
     browser.element(".header-search-input").click()
     browser.element(".header-search-input").send_keys("eroshenkoam/allure-example")
     browser.element(".header-search-input").submit()
